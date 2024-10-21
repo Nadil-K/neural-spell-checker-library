e6a117e845947267b8bb89eca02d01c7ece29c04
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2024-10-21 10:02:03 +0530

    entry point

---
+++ NeuralSpellChecker.py
@@ -0,0 +1,10 @@
+class NeuralSpellChecker:
+
+    def __init__(self, model):
+        # TODO: Initialize the model
+        pass
+
+
+    def check(self, text):
+        # TODO: Implement the spell checking
+        pass
